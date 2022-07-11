928a96b3c1fb44f34e284663a1d68051d654e469
wellhenrique <welissonh80@gmail.com>
2022-07-11 14:52:35 -0300

    feat: configure theme styled-components

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,13 +1,16 @@
+import { ThemeType } from "styled-components";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
   justify-content: center;
   align-items: center;
+
+  background-color: ${({ theme }) => theme.colors.background};
 `;
 
 export const Title = styled.Text`
   font-size: 24px;
   font-weight: bold;
-  color: purple;
+  color: ${({ theme }) => theme.colors.title};
 `;
