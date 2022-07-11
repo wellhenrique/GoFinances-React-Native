df8076bf2c9796e97c3bf309f5ee9b08a4e41910
wellhenrique <welissonh80@gmail.com>
2022-07-11 18:08:20 -0300

    feat: create highlight component

---
+++ src/components/HighlightCard/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View``;
+
+export const Header = styled.View``;
+export const Title = styled.View``;
+export const Icon = styled.View``;
+export const Footer = styled.View``;
+export const Amount = styled.View``;
+export const LastTransaction = styled.View``;
