7e539ddd3281b2a51222c7c9df3903fd090f0f13
wellhenrique <welissonh80@gmail.com>
2022-07-11 20:08:44 -0300

    feat: highlightCard stylized

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,10 +1,44 @@
 import styled from "styled-components/native";
+import { RFValue } from "react-native-responsive-fontsize";
+import { Feather } from "@expo/vector-icons";
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  background-color: ${({ theme }) => theme.colors.shape};
+  border-radius: 5px;
+  margin-top: -80px;
+  width: ${RFValue(300)}px;
+  padding: 19px 23px ${RFValue(42)}px;
+`;
 
-export const Header = styled.View``;
-export const Title = styled.View``;
-export const Icon = styled.View``;
+export const Header = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.regular};
+  font-size: ${RFValue(16)}px;
+
+  color: ${({ theme }) => theme.colors.text_dark};
+`;
+
+export const Icon = styled(Feather)`
+  font-size: ${RFValue(40)}px;
+`;
+
 export const Footer = styled.View``;
-export const Amount = styled.View``;
-export const LastTransaction = styled.View``;
+
+export const Amount = styled.Text`
+  font-family: ${({ theme }) => theme.fonts.medium};
+  font-size: ${RFValue(32)}px;
+  color: ${({ theme }) => theme.colors.text_dark};
+  margin-top: 38px;
+`;
+
+export const LastTransaction = styled.Text`
+  font-size: ${RFValue(12)}px;
+  font-family: ${({ theme }) => theme.fonts.regular};
+
+  color: ${({ theme }) => theme.colors.text};
+`;
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { HighlightCard } from "../../components/HighlightCard";
+
 import {
   Container,
   Header,
