7b718ee504334abbff0b47cbedee4b1e6895a072
wellhenrique <welissonh80@gmail.com>
2022-07-13 22:13:59 -0300

    feat: modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+export function Modal() {
+  return (
+    <Container style={styles.container}>
+      <Text>Modal</Text>
+    </Container>
+  );
+}
